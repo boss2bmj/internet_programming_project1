91e8c278cd272fafe4a51ea9c202a657d82c2095
Boss2bmj <boss2bmj@gmail.com>
2016-02-05 17:15:30 +0700

    finished add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,11 +66,10 @@ class ProductsController < ApplicationController
     def set_product
       @product = Product.find(params[:id])
     end
-
+    private
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price, :category, :subcategory, :stock)
     end
-    private
 
 end
