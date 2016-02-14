0dfc7f1d204bac6da2997b4c3526c558a70f755e
Boss2bmj <boss2bmj@gmail.com>
2016-02-14 21:13:23 +0700

    add annotate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  description        :text
+#  image_url          :string
+#  price              :integer
+#  category           :string
+#  subcategory        :string
+#  stock              :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#
+
 class Product < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
