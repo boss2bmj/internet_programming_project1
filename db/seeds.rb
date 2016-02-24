61b5976eeb1f8af574cf5f3148fad3bd030a02cd
Boss2bmj <boss2bmj@gmail.com>
2016-02-24 14:52:10 +0700

    not finish poly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+poi = Product.create(title: "Racket")
+
+poi_racket_info = PoiRacketInfo.create(title_name: "racket 1")
+poi_product_info = PoiProductInfo.create(product_id: poi.id, poi_info: poi_racket_info)
+
+
+
+poi_grips_info = PoiGripsInfo.create(title_name: "grip 1")
+
+# poi_product_info = PoiProductInfo.create(product_id: poi.id, poi_info_type: "PoiGripsInfo", poi_info_id: poi_grips_info.id)
